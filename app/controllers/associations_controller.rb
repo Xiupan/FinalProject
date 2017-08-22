c68a23483823703739b8be2979b68c07b5991949
Alan Hong <xiupan@gmail.com>
2017-08-22 13:39:11 -0500

    Trying to create associations between Users and Empires.

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,10 +1,12 @@
 class AssociationsController < ApplicationController
   def new
-    
+
   end
 
   def create
-
+    # find the logged in User and tie the selected Empire to that User
+    @empire = Empire.find_by id: params[:id]
+    
   end
 
   def destroy
