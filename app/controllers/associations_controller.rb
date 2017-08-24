d703529cfa4529c20c3e4928b451ce90edaacd54
Alan Hong <xiupan@gmail.com>
2017-08-24 11:27:54 -0500

    Form actually sets the logged User's empire correctly! Woohoo!

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -3,13 +3,18 @@ class AssociationsController < ApplicationController
 
   end
 
-  def create
+  def updateEmpireToUser
     # find the logged in User and tie the selected Empire to that User
-    @empire = Empire.find_by id: params[:id]
-    
+    @empire = Empire.find_by id: params[:name][:id]
+    @empire.user_id = session[:user_id]
+    @empire.save!
   end
 
   def destroy
 
+  end
+
+  def empire_params
+    params.require(:empire).permit(:id, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users
-  resources :associations, :only => [:create, :update, :destroy]
+  # resources :associations, :only => [:create, :update, :destroy]
+
+  post 'associations/update/' => 'associations#updateEmpireToUser'
 
   get 'sessions/new' => 'sessions#new', as: :login
   post 'sessions/new' => 'sessions#create'
